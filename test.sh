87d68feb4c39a3321e88e56b5bfe0ec83c53d821
Antti Korpimäki <antti.korpimaki@gmail.com>
2021-02-21 19:34:40 +0100

    Test getMonitorLayout return value

--- test.sh
+++ test.sh
@@ -27,7 +27,8 @@ hsetroot -solid "#000000"
 sleep 0.5
 echo '- - -'
 
-tmpfile="/tmp/hudkit_testfile.html"
+tmpfile_output="/tmp/hudkit_test_output.txt"
+tmpfile_html="/tmp/hudkit_test_input.html"
 echo '''
 <html>
 <body>
@@ -35,14 +36,23 @@ echo '''
 <style>
 body { background: rgba(255,255,255,0.5) }
 </style>
+<script>
+console.log("what")
+;(async () => {
+  console.log(JSON.stringify(await Hudkit.getMonitorLayout()))
+})()
+</script>
 </html>
-''' > $tmpfile
-echo "Wrote test page to $tmpfile as thus:"
-cat "$tmpfile"
+''' > $tmpfile_html
+echo "Wrote test page to $tmpfile_html as thus:"
+cat "$tmpfile_html"
 echo '- - -'
 
 echo "Starting Hudkit"
-./hudkit "file://$tmpfile" & hudkit_pid=$!
+./hudkit "file://$tmpfile_html" > "$tmpfile_output" 2>&1 & hudkit_pid=$!
+# We have to redirect stderr to stdout (2>&1), because webkit's
+# 'enable-write-console-messages-to-stdout' setting is a lie; it actually logs
+# to stderr.
 sleep 3
 echo '- - -'
 
@@ -62,14 +72,41 @@ kill "$xvfb_pid"
 wait "$xvfb_pid"
 echo '- - -'
 
+exit_code=0
+
 echo "Comparing pixel value"
 expected_value="#808080"
-rm "$tmpfile"
 if [ "$out" != "$expected_value" ]; then
     echo "Pixel didn't match!"
     echo "Expected $expected_value, got $out"
-    exit 1;
+    exit_code=1
 else
-    echo "Pixel matched!  Test passed."
-    exit 0
+    echo "Pixel matched!  OK."
 fi
+echo '- - -'
+
+echo "Comparing output log"
+expected_to_contain=$(cat <<END
+CONSOLE LOG [{"name":"screen","x":0,"y":0,"width":1280,"height":1024}]
+END
+)
+
+if grep --quiet --fixed-strings "$expected_to_contain" "$tmpfile_output"; then
+    echo "Saw Hudkit.getMonitorLayout() response in log!  OK."
+else
+    echo "Did not see Hudkit.getMonitorLayout() response in log!"
+    exit_code=1
+fi
+echo '- - -'
+
+echo "Removing temporary files"
+rm "$tmpfile_html"
+rm "$tmpfile_output"
+echo '- - -'
+
+if (( "$exit_code" > 0 )); then
+    echo "Some tests failed."
+else
+    echo "All tests passed.  :)"
+fi
+exit "$exit_code"
